6a972e3f6887b06a609c0e9fdc018346c40d89af
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2021-09-30 03:30:50 +0300

    Fix handling of WP nightly

--- install-wp.sh
+++ install-wp.sh
@@ -20,8 +20,17 @@ download_wp() {
 	fi
 
 	if [ ! -d "/wordpress/wordpress-${VERSION}" ]; then
-		mkdir -p "/wordpress/wordpress-${VERSION}"
-		wget -q "https://wordpress.org/wordpress-${VERSION}.tar.gz" -O - | tar --strip-components=1 -zxm -f - -C "/wordpress/wordpress-${VERSION}"
+		if [ "${VERSION}" = "nightly" ]; then
+			cd /wordpress
+			wget -q https://wordpress.org/nightly-builds/wordpress-latest.zip
+			unzip wordpress-latest.zip
+			mv /wordpress/wordpress /wordpress/wordpress-nightly
+			rm -f wordpress-latest.zip
+			cd -
+		else
+			mkdir -p "/wordpress/wordpress-${VERSION}"
+			wget -q "https://wordpress.org/wordpress-${VERSION}.tar.gz" -O - | tar --strip-components=1 -zxm -f - -C "/wordpress/wordpress-${VERSION}"
+		fi
 		wget -q https://raw.github.com/markoheijnen/wp-mysqli/master/db.php -O "/wordpress/wordpress-${VERSION}/wp-content/db.php"
 	else
 		echo "Skipping WordPress download"
